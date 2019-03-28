8f7267f7b258bbe93b00ac479116cd76a7575466
nuaSiekier <wiktor.siekierka@gmail.com>
2019-03-28 17:12:31 +0100

    bierz to kurła

--- forecast.js
+++ forecast.js
@@ -1,16 +1,35 @@
 window.onload = function () {
-    var showForecast = document.getElementById('forecastLocal');
+    // var showForecast = document.getElementById('forecastLocal');
+    var cityName = document.getElementById('cityNameInfo');
+    var cityInfo = document.getElementById('infoAboutCity');
+    var temperature = document.getElementById('tempInfo');
+    var percipitation = document.getElementById('rainChance');
+    var wind = document.getElementById('windSpeed');
+
     loadForecast();
+
+
+
     function loadForecast() {
+
         $.ajax({
             url: 'https://api.openweathermap.org/data/2.5/forecast?lat=49.8810900&lon=20.0893600&APPID=03a706d4e9b3ce5dd1105d1704fa5835'
         })
             .done(function (res) {
+
+
+                cityName.innerHTML = res.city.name;
+
                 for (i = 0; i < res.list.length; i++) {
-                    var nextFor = document.createElement('li');
-                    nextFor.innerHTML = res.list[i].dt_txt + ": " + res.city.name;
-                    showForecast.appendChild(nextFor);
+                    cityInfo.innerHTML = res.list[0].dt_txt;
+                    temperature.innerHTML = Math.round(res.list[0].main.temp - 273.15);
+                    percipitation.innerHTML = res.list[0].main.humidity + '% humidity';
+                    wind.innerHTML = res.list[0].wind.speed + ' km/h';
                 }
+
+
+
+
             });
     }
 }
